49958eeeaee5af82a76c90477af037578fcb1496
Sundström Valter <mail@valter.nu>
2022-05-30 00:47:11 +0200

    ...

--- led_matrix.h
+++ led_matrix.h
@@ -1,14 +1,16 @@
-#include <MD_MAX72xx.h>
+#ifndef LED_MATRIX_H_M4JY8R3Y
+#define LED_MATRIX_H_M4JY8R3Y
 
+namespace LedMatrix {
+void setup();
+void loop();
 
-namespace LedMatrix{
-  void setup();
-  void loop();
+/* void set_message(const String &str); */
+void set_rows(int start_y, int count, int state);
+void set_cols(int start_x, int count, int state);
+void set_point(int col, int row, int state);
+void fill(bool on);
+void update();
+} // namespace LedMatrix
 
-  /* void set_message(const String &str); */
-  void set_rows(int start_y, int count, int state);
-  void set_cols(int start_x, int count, int state);
-  void set_point(int col, int row, int state);
-  void fill(bool on);
-  void update();
-}
+#endif /* end of include guard: LED_MATRIX_H_M4JY8R3Y */
